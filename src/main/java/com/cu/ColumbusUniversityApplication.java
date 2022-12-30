a13b973262e5e579b84a49a4d985725a8a3c5fbd
Miraaj Khan <miraajk16@gmail.com>
2022-12-30 17:23:43 +0530

    conflict

---
+++ src/main/java/com/cu/ColumbusUniversityApplication.java
@@ -0,0 +1,13 @@
+package com.cu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColumbusUniversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColumbusUniversityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cu/ColumbusUniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColumbusUniversityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
